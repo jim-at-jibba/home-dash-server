2cb5002dff935654fcd16f32baf3a6b01f0c80d6
James Best <jim@justjibba.net>
2021-05-04 04:43:52 +0100

    Fix topic resolver

--- src/features/mqtt/mqtt-message.ts
+++ src/features/mqtt/mqtt-message.ts
@@ -7,15 +7,16 @@ const MqttMessageUnion = createUnionType({
   name: "Mqtt",
   types: () => [EnviroMessage, SwitchMessage, TemperatureMessage] as const,
   resolveType: (value) => {
-    if (value.topic === "enviro") {
+    if (value.topic.includes("enviro")) {
       return EnviroMessage
     }
-    if (value.topic === "switch") {
+    if (value.topic.includes("switch")) {
       return SwitchMessage
     }
-    if (value.topic === "temperature") {
+    if (value.topic.includes("temperature")) {
       return TemperatureMessage
     }
+
     return undefined
   },
 })
