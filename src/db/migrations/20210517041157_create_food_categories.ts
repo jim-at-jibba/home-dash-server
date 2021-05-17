686d880117d8d8c64bc20543146cd4a40aec44c4
James Best <jim@justjibba.net>
2021-05-17 05:19:54 +0100

    Add courses and categories migrations

---
+++ src/db/migrations/20210517041157_create_food_categories.ts
@@ -0,0 +1,13 @@
+import {Knex} from "knex"
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable("food_categories", (t) => {
+    t.uuid("id")
+    t.string("food_category_name").notNullable()
+    t.timestamps(false, true)
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.raw("DROP TABLE food_categories CASCADE")
+}
