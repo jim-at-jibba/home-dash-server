de3a647a899cf7adbec7a23d2dc2616acf1658ff
James Best <jim@justjibba.net>
2021-05-17 05:59:39 +0100

    Add ingredients and steps migrations

---
+++ src/db/migrations/20210517044046_create_recipe_ingredients.ts
@@ -0,0 +1,18 @@
+import {Knex} from "knex"
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable("recipe_ingredients", (t) => {
+    t.uuid("id").unique().primary()
+    t.uuid("recipe_id").notNullable()
+    t.string("ingredient").notNullable()
+    t.timestamps(false, true)
+
+    t.unique(["recipe_id"])
+
+    t.foreign("recipe_id").references("id").inTable("recipes")
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.raw("DROP TABLE recipe_ingredients CASCADE")
+}
