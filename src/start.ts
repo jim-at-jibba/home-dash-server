e1ca7942374add9fae42dd04d5367d9526c86a98
James Best <jim@justjibba.net>
2021-05-22 03:56:58 +0100

    Fix env injection

--- src/start.ts
+++ src/start.ts
@@ -2,6 +2,7 @@ import "reflect-metadata"
 import {MQTT} from "./mqtt"
 import db from "./db/connection"
 import createServer from "./server"
+require("dotenv").config()
 
 const startServer = async ({port = process.env.PORT} = {}) => {
   db.migrate.latest({loadExtensions: [".js", ".ts"]})
