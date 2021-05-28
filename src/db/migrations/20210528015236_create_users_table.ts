eb6f02ba9dd3af505729024b7fc4a8d5147fbd8f
James Best <jamesbest@Jamess-MacBook-Pro.local>
2021-05-28 03:06:24 +0100

    Save migration :face-palm

--- src/db/migrations/20210528015236_create_users_table.ts
+++ src/db/migrations/20210528015236_create_users_table.ts
@@ -1,10 +1,15 @@
-import { Knex } from "knex";
-
+import {Knex} from "knex"
 
 export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable("users", (t) => {
+    t.uuid("id")
+    t.string("username").notNullable().unique()
+    t.string("email").notNullable().unique()
+    t.string("password").notNullable()
+    t.timestamps(false, true)
+  })
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  return knex.raw("DROP TABLE users CASCADE")
 }
-
